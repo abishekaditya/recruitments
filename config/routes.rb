e10166121eec8696ff7912c0f76f6822833faad6
abishekaditya <abishekaditya@outlook.com>
2016-09-13 22:02:22 +0530

    Finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,25 @@ Rails.application.routes.draw do
 
   root 'users#new'
 
-  get 'selections/design'
-  get 'selections/management'
-  get 'selections/technical'
+ # get 'selections/design'
+ # get 'selections/management'
+ # get 'selections/technical'
 
   get '/signup' => 'users#new'
 
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
+ # get '/login' => 'sessions#new'
+ # post '/login' => 'sessions#create'
 
-  get '/logout' => 'sessions#destroy'
+#  get '/logout' => 'sessions#destroy'
 
-  post '/users' => 'users#create'
+#  post '/users' => 'users#create'
 
-  get '/selections' => 'selections#base'
+#  get '/selections' => 'selections#base'
 
-  post '/selections/man_post' => 'selections#man_post'
+#  post '/selections/man_post' => 'selections#man_post'
 
-  post '/selections/des_post' => 'selections#des_post'
+#  post '/selections/des_post' => 'selections#des_post'
 
-  post '/selections/tech_post' => 'selections#tech_post'
+#  post '/selections/tech_post' => 'selections#tech_post'
 
 end
